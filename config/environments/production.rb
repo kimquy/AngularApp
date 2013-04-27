464991105f503580e67d0a3d440359984cef5680
long nguyen <nguy1461@vandals.uidaho.edu>
2013-04-27 15:49:09 -0700

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ RandomNameWeb::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false 
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor  = :uglifier
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -41,7 +41,7 @@ RandomNameWeb::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
-
+  config.assets.enabled = true
   # Set to :debug to see everything in the log.
   config.log_level = :info
 
