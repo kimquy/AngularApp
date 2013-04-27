9cb9bc3b3002a40a320d92e111fe0d18efea3930
long nguyen <nguy1461@vandals.uidaho.edu>
2013-04-27 15:41:04 -0700

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ RandomNameWeb::Application.configure do
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
