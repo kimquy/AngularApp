25f70c4484c7c79c8cb2a9c94dfd0e59c59668b7
long nguyen <nguy1461@vandals.uidaho.edu>
2013-04-27 20:45:19 -0700

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ RandomNameWeb::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false 
+  config.serve_static_assets = true 
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor  = :uglifier
@@ -28,7 +28,7 @@ RandomNameWeb::Application.configure do
   config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
