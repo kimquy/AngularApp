eb96f18d86c57b0ee93978cfe350eac021a4df83
long nguyen <nguy1461@vandals.uidaho.edu>
2013-04-27 20:56:35 -0700

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 RandomNameWeb::Application.configure do
-  include Sprockets::LazyCompressor
+  include Sprockets
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
