d100417023722106a5d8637eaa2d722dc7ecc855
long nguyen <nguy1461@vandals.uidaho.edu>
2013-04-27 20:28:11 -0700

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,9 @@ RandomNameWeb::Application.configure do
   config.serve_static_assets = false 
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor  = :uglifier
+  # config.assets.js_compressor  = :uglifier
   # config.assets.css_compressor = :sass
+  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
